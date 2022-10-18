b09f03372b1005056472cb94af76177e45a73f29
EidHachem <eidhachem1@gmail.com>
2022-10-18 11:57:01 +0300

    Install swal 2 and apply popup in categories page

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Swal from 'sweetalert2';
 import { checkStatus } from '../redux/categories/categories';
 import Button from './UI/Button';
 import classes from '../Styles/Categories.module.css';
 
 export default function Categories() {
   const status = useSelector((state) => state.categories);
+  const [checked, setChecked] = React.useState(false);
   const dispatch = useDispatch();
   const checkinStatusHandler = () => {
     dispatch(checkStatus());
+    if (status && checked) {
+      Swal.fire({
+        title: 'Please try again later',
+        icon: 'info',
+        confirmButtonText: 'Ok',
+      });
+    }
+    setChecked(true);
   };
 
   return (
     <div className={classes.container}>
       <p className={classes.text}>{status}</p>
-      <Button content="Check status" onClick={checkinStatusHandler} />
+      <Button content="Check status" onClick={checkinStatusHandler} margin="0" />
     </div>
   );
 }
